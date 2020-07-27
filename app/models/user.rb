2b5814a1a86c508675e38563776df73315237e17
Joao Cardoso <joao.cardoso@yale.edu>
2020-07-27 16:36:09 -0400

    fake login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
     has_many :user_robots
     has_many :robots, through: :user_robots
 
+    validates :user_name, presence: true
+    validates :user_name, uniqueness: true
+
     def create_robot(name)
         robot = Robot.create(name: name)
         robot.author = self.user_name
